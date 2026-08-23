421356a662b464778ba2f4c6cdab43d9ee6de1d5
San Sovath <sansovath0@gmail.com>
2026-08-23 22:19:37 +0700

    Test components

--- frontend/phone-shop/src/components/Navbar.js
+++ frontend/phone-shop/src/components/Navbar.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-red-200 w-full p-4'></div>
+    <div className='bg-red-200 w-full p-4'>
+      <p>i love nigga</p>
+    </div>
   )
 }
 
