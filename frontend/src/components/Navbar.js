6f0eef8f6530eeb40840dd36b6f2883963416d29
San Sovath <sansovath0@gmail.com>
2026-08-24 22:23:43 +0700

    change component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='bg-red-200 w-full p-4'>
-      <p>i love nigga</p>
+      <p>Lorem ipsum dolor sit amet</p>
     </div>
   )
 }
