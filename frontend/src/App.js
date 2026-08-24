f7684b63e439d5bc806716ced679910b96e6899c
heang <laolimheang1@gmail.com>
2026-08-24 22:34:07 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
+      <Home />
       <header className="App-header">
         <Navbar />
       </header>
